48bfacc45beecb2a700b7efb6a017dad47d56071
Curtis Trieu <trieulootech@gmail.com>
2023-09-04 05:03:02 -0700

    update

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let repsonse = await fetch("https://d1lrdw2mln1cta.cloudfront.net/")
+    let repsonse = await fetch("https://3exoarfhfyfy5ooxvx45rpxcom0truhf.lambda-url.us-west-2.on.aws/")
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
     
