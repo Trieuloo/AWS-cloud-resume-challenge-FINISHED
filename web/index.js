1dd619edc22a0a908f2d5994c83b6ad92d6d9b35
Trieuloo <132096069+Trieuloo@users.noreply.github.com>
2023-09-04 22:41:28 -0700

    Update index.js

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://3exoarfhfyfy5ooxvx45rpxcom0truhf.lambda-url.us-west-2.on.aws/")
+    let response = await fetch("Filler")
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
     
